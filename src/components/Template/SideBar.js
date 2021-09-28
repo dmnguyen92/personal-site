938b6916a6dcc5f30141360dd75866f4dca155a4
Dang Minh Nguyen <dangminh.nguyen@shopee.com>
2021-09-28 18:17:00 +0800

    Update contact info and About Me

--- src/components/Template/SideBar.js
+++ src/components/Template/SideBar.js
@@ -12,21 +12,19 @@ const SideBar = () => (
         <img src={`${PUBLIC_URL}/images/me.jpg`} alt="" />
       </Link>
       <header>
-        <h2>Michael D&apos;Angelo</h2>
-        <p><a href="mailto:michael.l.dangelo@gmail.com">michael.l.dangelo@gmail.com</a></p>
+        <h2>Nguyen Dang Minh</h2>
+        <p><a href="mailto:dmnguyen92@gmail.com">dmnguyen92@gmail.com</a></p>
       </header>
     </section>
 
     <section className="blurb">
       <h2>About</h2>
-      <p>Hi, I&apos;m Michael. I like building things.
-        I am a <a href="https://icme.stanford.edu/">Stanford ICME</a> graduate, YC Alumni, and
-        the co-founder and CTO of <a href="https://arthena.com">Arthena</a>. Before Arthena I was
-        at <a href="https://matroid.com">Matroid</a>
-        , <a href="https://planet.com">Planet</a>
-        , <a href="https://planetaryresources.com">Planetary Resources</a>
-        , <a href="https://facebook.com">Facebook</a>
-        , and <a href="https://seds.org">SEDS</a>.
+      <p>Hi, I&apos;m Dang Minh. I am a Machine Learning Engineer
+        who likes to learn and create
+        new things. I got my Bachelor and PhD degree from
+        <a href="https://ntu.edu.sg/">Nanyang Technological University</a>.
+        Currently I am working with problems about Recommendataion System
+        and Natural Language Processing.
       </p>
       <ul className="actions">
         <li>
@@ -37,7 +35,6 @@ const SideBar = () => (
 
     <section id="footer">
       <ContactIcons />
-      <p className="copyright">&copy; Michael D&apos;Angelo <Link to="/">mldangelo.com</Link>.</p>
     </section>
   </section>
 );
